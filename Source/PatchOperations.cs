d77a7f1092f6edbaab26e35a9225779e22f19b96
ZzZombo <ZzZombo@users.noreply.github.com>
2021-04-14 11:02:50 +0800

    -Changed the patch operation to a better name.

--- Source/PatchOperations.cs
+++ Source/PatchOperations.cs
@@ -9,7 +9,7 @@ internal static class MOD
 
 namespace ZzZomboRW
 {
-	public class PatchOperationAddNode: PatchOperationPathed
+	public class PatchOperationAddNodeWithDefault: PatchOperationPathed
 	{
 		private string name;
 		private XmlContainer value;
